fb5d6bdb75505d74904c97781d71ab42a1f33ff9
NatnaelTaddese <natnaeltaddese3@gmail.com>
2026-02-08 19:11:43 +0800

    Preload dotenv in dev script and fix auth imports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { Elysia } from "elysia";
-import { auth } from "@/src/auth";
+import { auth } from "./auth";
 
 export default new Elysia().mount(auth.handler).get("/", () => "Hello Elysia");
