f4c8a172c40eda7afb7c4682fe8089cf9ad1a79e
NatnaelTaddese <natnaeltaddese3@gmail.com>
2026-02-08 19:04:26 +0800

    Export Elysia app directly

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import { Elysia } from "elysia";
 import { auth } from "@/src/auth";
 
-const app = new Elysia().mount(auth.handler).get("/", () => "Hello Elysia");
-
-export default app;
+export default new Elysia().mount(auth.handler).get("/", () => "Hello Elysia");
